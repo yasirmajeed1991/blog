ca28cc44dd316ff9c618b4a31b3bf3bcc1ba5320
Yasir Majeed <112022252+yasirmajeed1991@users.noreply.github.com>
2023-08-02 22:55:42 +0500

    Updated

---
+++ blogadmin/libs/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","root","pwdpwd","bukola_blog");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ ?>
